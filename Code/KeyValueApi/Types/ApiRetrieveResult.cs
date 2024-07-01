691067e411cc4eb73408454457805b1d9dad8453
Simon Randby <simon.randby@gmail.com>
2024-07-01 22:07:46 +0200

    Remove unused files

--- Code/KeyValueApi/Types/ApiRetrieveResult.cs
+++
@@ -1,8 +0,0 @@
-public record ApiRetrieveResult
-{
-    public required string ValueB64 { get; init; }
-    public Dictionary<string, string>? KafkaHeaders { get; init; }
-    public required string CorrelationId { get; init; }
-    public required string KafkaEventMetadata { get; init; }
-}
-
--- Code/KeyValueApi/Types/KafkaEventMetadata.cs
+++
@@ -1,7 +0,0 @@
-public record KafkaEventMetadata
-{
-    public required string Topic { get; init; }
-    public required long Partition { get; init; }
-    public required long Offset { get; init; }
-    public required string TopicTimestamp { get; init; }
-}
